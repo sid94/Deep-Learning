15ffcdf54fdb56ff699abd2e881502f6e8fb0b1b
Shweta94Me <mestryshweta30@gmail.com>
2020-05-28 21:56:38 -0400

    Pred printing updated

--- prediction.py
+++ prediction.py
@@ -53,5 +53,5 @@ def message_to_array(msg):
 
 custom_msg_ham = 'Subject: cut your medic @ l costs by 65 % on brand name medic @ tions . cut your medic @ l costs by 65 % on brand name medic @ tions . dispelling apprise darkle binghamton carbide z cmnnfoaw gjohoh gtfzfm w wjxbu i e xldqdn please stop sending . . . . . . . . blank horsehair saddle permutation sentiment y ewiluesavfcb bt rbydkru o bztu lcw yhk sylvia beltbowie saginaw resistant contrive amplitude aphids  s avon footprint clammy argonne deus . - - - - - begin pgp signature - - - - - version : pgp 8 . 0 . 2 - not licensed for commercial use : www . pgp . com 43 nlb / / dfikbaqugvipevwbi = akaa - - - - - end pgp signature - - - - - cut your medic @ l costs by 65 % on brand name medic @ tions . 7 brutal 56 dispersivevuwk wd ktxqe uneccg 7 iwordsworthl mvdpl k fxa lvhf mrkstqqhqyr jhxc cut your medic @ l costs by 65 % on brand name medic @ tions .'
 test_seq = message_to_array(custom_msg_ham)
-pred = model.predict_classes(test_seq)
+pred = model.predict_classes(test_seq)[0][0]
 print(pred)
