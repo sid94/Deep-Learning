2589ccd608d5089f9e83bdc0e1066331525061f4
Shweta94Me <mestryshweta30@gmail.com>
2020-05-29 01:12:05 -0400

    prediction done using JSON model file

--- api.py
+++ api.py
@@ -1,10 +1,12 @@
 from flask import Flask,render_template,url_for,request
 import os
 import numpy as np
-from keras.preprocessing.text import Tokenizer
-from keras.preprocessing.sequence import pad_sequences
-from keras.models import load_model
-from keras.models import model_from_json
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+from tensorflow.keras.models import load_model
+from tensorflow.keras.models import model_from_json
+
+
 app = Flask(__name__)
 
 a =  "Hello Flask"
@@ -12,15 +14,15 @@ a =  "Hello Flask"
 max_vocab = 15000
 max_len = 700
 
-json_file = open('model.json', 'r')
-loaded_model_json = json_file.read()
-json_file.close()
-loaded_model = model_from_json(loaded_model_json)
-# load weights into new model
-loaded_model.load_weights("model.h5")
-print("Loaded model from disk")
-loaded_model.save('model.hdf5')
-loaded_model = load_model('model.hdf5')
+# json_file = open('model.json', 'r')
+# loaded_model_json = json_file.read()
+# json_file.close()
+# loaded_model = model_from_json(loaded_model_json)
+# # load weights into new model
+# loaded_model.load_weights("model.h5")
+# print("Loaded model from disk")
+# loaded_model.save('model.hdf5')
+loaded_model = load_model('model.h5')
 
 #loaded_model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
 
@@ -47,8 +49,7 @@ def detect():
         
         my_prediction = loaded_model.predict_classes(testdata)[0][0]
         # return render_template('result.html', prediction=my_prediction)
-        print(my_prediction)
-    # return "My prediction:" + list(my_prediction)
+    return "My prediction:" + str(my_prediction)
 
     
 if __name__ == "__main__":
--- prediction.py
+++ prediction.py
@@ -58,9 +58,12 @@ print("Test loss is {0:.2f} accuracy is {1:.2f}" .format(acc[0],acc[1]))
 # print(pred)
 
 # serialize model to JSON
-model_json = model.to_json()
-with open("model.json", "w") as json_file:
-    json_file.write(model_json)
-# serialize weights to HDF5
-model.save_weights("model.h5")
+# model_json = model.to_json()
+# with open("model.json", "w") as json_file:
+#     json_file.write(model_json)
+# # serialize weights to HDF5
+# model.save_weights("model.h5")
+# print("Saved model to disk")
+
+model.save('model.h5')
 print("Saved model to disk")
