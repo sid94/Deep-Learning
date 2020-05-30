9670b8f85628395a8db54cefeb76b8019f49512e
SidKolhapure <sid.kolhapure94@gmail.com>
2020-05-30 18:50:22 -0400

    added react page and post method to post data

--- api.py
+++ api.py
@@ -1,11 +1,11 @@
-from flask import Flask,render_template,url_for,request,json
+from flask import Flask,render_template,url_for,request,json,jsonify
 import os
 import numpy as np
 from tensorflow.keras.preprocessing.text import Tokenizer
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 from tensorflow.keras.models import load_model
 from tensorflow.keras.models import model_from_json
-
+import sys
 
 app = Flask(__name__)
 
@@ -14,28 +14,19 @@ a =  "Hello Flask"
 max_vocab = 15000
 max_len = 700
 
-# json_file = open('model.json', 'r')
-# loaded_model_json = json_file.read()
-# json_file.close()
-# loaded_model = model_from_json(loaded_model_json)
-# # load weights into new model
-# loaded_model.load_weights("model.h5")
-# print("Loaded model from disk")
-# loaded_model.save('model.hdf5')
 loaded_model = load_model('model.h5')
-
-#loaded_model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
 
 @app.route("/")
 def home():
-    # return render_template('home.html')
-    return 'Shweta home'
+    return render_template('index.html')
 
 @app.route("/detect", methods=["POST"])
 def detect():
     my_prediction = ""
     req_data = request.get_json()
-    message = req_data['data']
+    data = json.loads(request.data.decode('UTF-8'))
+    message = data['data']
+    res = {}
     if(message):
         testtext = []
         testtext.append(message)
@@ -45,7 +36,8 @@ def detect():
         testdata = pad_sequences(testseq, maxlen=max_len)
         my_prediction = loaded_model.predict_classes(testdata)[0][0]
         #my_prediction = np.argmax(loaded_model.predict(testdata), axis=-1)[0]
-    return "My prediction:" + str(my_prediction)
+        res['prediction'] = str(my_prediction)
+    return jsonify(res)
 
     
 if __name__ == "__main__":
