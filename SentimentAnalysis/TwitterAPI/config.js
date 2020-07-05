757898a9b6b78b0b26148f1282c7ec90922e6dad
Shweta94Me <mestryshweta30@gmail.com>
2020-07-05 01:06:58 -0400

    gitignore updated

--- SentimentAnalysis/TwitterAPI/config.js
+++
@@ -1,7 +0,0 @@
-//Purpose:::Saved configuration information related to Twitter Developer Account
-module.exports = {
-    consumer_key:'YOUR_CONSUMER_KEY',
-    consumer_secret:'YOUR_CONSUMER_SECRET_KEY',
-    access_token_key:'YOUR_ACCESS_TOKEN_KEY',
-    access_token_secret:'YOUR_ACCESS_TOKEN_SECRET'
-}
