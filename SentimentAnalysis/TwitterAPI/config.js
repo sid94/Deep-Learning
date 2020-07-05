41ce98dd1a31c4c3490cb07a51f7ebafe54bffd7
Shweta94Me <mestryshweta30@gmail.com>
2020-07-05 00:38:42 -0400

    Config file updated

--- SentimentAnalysis/TwitterAPI/config.js
+++ SentimentAnalysis/TwitterAPI/config.js
@@ -1,7 +1,7 @@
 //Purpose:::Saved configuration information related to Twitter Developer Account
 module.exports = {
-    consumer_key:'P3qOeX5luAuKFxcMJvhP7RYBA',
-    consumer_secret:'0lBHkESSb0PEENyPSE57a8bq8gxfwd7Jc5g5x5TwOKSMjV7QEX',
-    access_token_key:'1278782635066691591-ejjkw3Bbhv27kEcEihoo2Om09utkgL',
-    access_token_secret:'l2URpXvBIwBBL7fykULt0frosUR4OSZC5xAsVGvutijck'
+    consumer_key:'YOUR_CONSUMER_KEY',
+    consumer_secret:'YOUR_CONSUMER_SECRET_KEY',
+    access_token_key:'YOUR_ACCESS_TOKEN_KEY',
+    access_token_secret:'YOUR_ACCESS_TOKEN_SECRET'
 }
