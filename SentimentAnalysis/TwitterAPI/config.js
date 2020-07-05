650390024eee170588412f738973e3daf55f248b
Shweta94Me <mestryshweta30@gmail.com>
2020-07-05 00:12:31 -0400

    Streaming API

---
+++ SentimentAnalysis/TwitterAPI/config.js
@@ -0,0 +1,7 @@
+//Purpose:::Saved configuration information related to Twitter Developer Account
+module.exports = {
+    consumer_key:'P3qOeX5luAuKFxcMJvhP7RYBA',
+    consumer_secret:'0lBHkESSb0PEENyPSE57a8bq8gxfwd7Jc5g5x5TwOKSMjV7QEX',
+    access_token_key:'1278782635066691591-ejjkw3Bbhv27kEcEihoo2Om09utkgL',
+    access_token_secret:'l2URpXvBIwBBL7fykULt0frosUR4OSZC5xAsVGvutijck'
+}
