0f7858535dd3a475b7f636c5f7e99278ab5fd2c8
SidKolhapure <sid.kolhapure94@gmail.com>
2020-07-15 20:53:06 -0400

    updated docker file for DL sentiment API and added a docker for Tweet Processor

--- SentimentAnalysis/TweetProcessor/config.js
+++ SentimentAnalysis/TweetProcessor/config.js
@@ -3,7 +3,9 @@ require('dotenv').config();
 const config = {
     KafkaHost: process.env.KAFKA_HOST || 'localhost:9092',
     KafkaTopic: process.env.KAFKA_TOPIC || 'tweetstream',
-    KafkaProcessedTweetTopic: process.env.KAFKA_PROCESSED_TOPIC || 'processedData'
+    KafkaProcessedTweetTopic: process.env.KAFKA_PROCESSED_TOPIC || 'processedData',
+    sentimentAnalyzerEndPoint: process.env.SENTIMENT_API_ENDPOINT || 'http://localhost:5000/predict',
+    tweetStoreServiceEndPoint: process.env.TWEET_STORE_ENDPOINT || 'http://localhost:4000/tweets'
 };
 
 module.exports = config
--- SentimentAnalysis/TweetProcessor/producer.js
+++ SentimentAnalysis/TweetProcessor/producer.js
@@ -10,27 +10,6 @@ const producer = new Producer(client, {
     partitionerType: 2
 });
 
-// const pushDataToKafka = () => {
-//     let dataToPush = response;
-//     try {
-//         let payloadToKafkaTopic = [{
-//             topic: config.KafkaProcessedTweetTopic,
-//             messages: JSON.stringify(dataToPush.data)
-//         }];
-//         console.log("Process Producer: "+ dataToPush.data[0]._id);
-//         producer.on('ready', function () {
-//             console.log("kafka producer is ready")
-//         })
-//         producer.send(payloadToKafkaTopic, (err, data) => {
-//         });
-//         producer.on('error', function (err) {
-//             //  handle error cases here
-//         })
-//     } catch (error) {
-//         console.log(error);
-//     }
-
-// };
 
 pushDataToKafka = function (response){
     let dataToPush = response.data;
