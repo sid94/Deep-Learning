aea1e01fe6083128ba40d379bc3183f376dd454e
Shweta94Me <mestryshweta30@gmail.com>
2020-07-18 00:19:07 -0400

    added new docker and docker compose files

--- SentimentAnalysis/UI/server/index.js
+++ SentimentAnalysis/UI/server/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 let server = require('http').createServer(app);
 let io = require('socket.io').listen(server);
 const EE = require('./consumerUI');
@@ -12,7 +12,7 @@ app.use('/', express.static('./dist', {
 }));
 
 io.on('connection', function(socket) {
-  //try {
+  try {
     console.log('Client Connected');
     EE.on('tweetStream', function (tweet) {
       socket.emit('tweet', tweet);
@@ -21,39 +21,7 @@ io.on('connection', function(socket) {
       console.error(`Error : ${error}`);
     });
     socket.on("disconnet", () => console.log("Client disconnected!"));
-  // } catch (error) {
-  //   console.error(`Error : ${error}`);
-  // }
+  } catch (error) {
+    console.error(`Error : ${error}`);
+  }
 });
-
-
-// io.on('connection', (socket) => {
-//   try {
-//     EE.on('tweetStream', function(tweet) {
-//       console.log(`tweetText : ${tweet}`)
-//       socket.emit('tweet', tweet);
-//     });
-
-//     EE.on('error', function(error) {
-//         throw error;
-//     });
-//     // consumer.on('message', function (message) {
-//     //   //Test the consumer message id
-//     //   msg = JSON.parse(message.value)
-//     //   console.log(msg[0]._id)
-
-//     //   if (message.value != undefined && message.value.length > 0) {
-//     //     message.value.forEach((elem) => {
-//     //       socket.emit('tweet', elem.text)
-//     //     })
-//     //   }
-//     // })
-//     // consumer.on('error', function (error) {
-//     //   //  handle error 
-//     //   console.log('error', error);
-//     // });
-//   } catch (error) {
-//     console.error(`Error : ${error.code}`);
-//   }
-//   socket.on("disconnet", () => console.log("Client disconnected!"));
-// });
